c1bde9c0742a8ce83c5f449bb9eac97774958d44
MLopez <codermex.web@gmail.com>
2024-02-28 06:34:21 -0300

    Creando funciones

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Varibles
 
 const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');//donde se van a colocar los elementos
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
@@ -16,7 +16,23 @@ function cargarEventListeners(){
 function agregarCurso(e){
     e.preventDefault(); //prevenimos la accion por default, como tenia un enlace que no se dirige a nada automaticamente se nos va al inicio y no queda en la misma posicion donde estamos
     if(e.target.classList.contains('agregar-carrito')){//Valida que solo se ejecute cuando toque esa clase
-        console.log('agregando al carrito los cursos...');
-    }
-    
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        //console.log('agregando al carrito los cursos...');
+        //console.log(cursoSeleccionado);
+        leerDatosCursos(cursoSeleccionado);
+    }   
 }
+
+//Lee el contenido del HTML al que le damos click y extrae la informacion del curso
+
+function leerDatosCursos(curso){
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+    }
+    console.log(infoCurso)
+}
+
